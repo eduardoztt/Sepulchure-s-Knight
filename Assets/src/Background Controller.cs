f6900594ec70d774303e3aa01810b82fc6322761
eduardoztt <edu.zancket@gmail.com>
2025-04-15 20:56:41 -0300

    arrumado cinemachine

--- Assets/src/Background Controller.cs
+++ Assets/src/Background Controller.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    private float startPos;
+    private float startPos, length;
     public Transform Cam;
     public float parallaxEffect;
 
     void Start()
     {
         startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float distance = Cam.position.x * parallaxEffect;
+        float movement = Cam.position.x *(1-parallaxEffect);
+
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+
+        if(movement > startPos + length){
+
+            startPos += length;
+            
+        }else{
+            if(movement < startPos - length){
+                startPos -= length;
+            }
+        }
     }
 }
-
-
-
-
-
-
-
