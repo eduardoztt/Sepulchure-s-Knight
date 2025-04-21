02daab8a4b5e2104fee5e02d2182e48791ecaa2b
Bruno Battisti <brunonb123456@gmail.com>
2025-04-21 19:17:01 -0300

    ajustes nas animacoes de pulo e ataque

--- Assets/src/PlayerControler.cs
+++ Assets/src/PlayerControler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,43 +7,44 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] private int velocidade = 5;
+    [SerializeField] private float jumpForce = 600;
 
     [SerializeField] private Transform charactereFeet;
     [SerializeField] private LayerMask groundLayer;
 
     private bool inTheGround;
+    private bool isAttacking = false;
+
     private Animator animator;
     private SpriteRenderer spriteRender;
 
     private int movingHash = Animator.StringToHash("moving");
     private int jumpingHash = Animator.StringToHash("jumping");
-    private int attackHash = Animator.StringToHash("attack");
+    private int attackingHash = Animator.StringToHash("attack");
+
+    [SerializeField] private float attackDuration = 0.5f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRender = GetComponent<SpriteRenderer>();
-    } 
+    }
 
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) && inTheGround)
+        if (Input.GetKeyDown(KeyCode.Space) && inTheGround && !isAttacking)
         {
-            rb.AddForce(Vector2.up * 600);
-        }
-
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            animator.SetTrigger(attackHash);
+            rb.AddForce(Vector2.up * jumpForce);
         }
 
         inTheGround = Physics2D.OverlapCircle(charactereFeet.position, 0.5f, groundLayer);
 
         animator.SetBool(movingHash, horizontalInput != 0);
         animator.SetBool(jumpingHash, !inTheGround);
+        animator.SetBool(attackingHash, isAttacking);
 
         if (horizontalInput > 0)
         {
@@ -52,6 +54,22 @@ public class PlayerController : MonoBehaviour
         {
             spriteRender.flipX = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.X) && !isAttacking)
+        {
+            StartCoroutine(AttackCoroutine());
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        isAttacking = true;
+        animator.SetBool(attackingHash, true);
+
+        yield return new WaitForSeconds(attackDuration);
+
+        isAttacking = false;
+        animator.SetBool(attackingHash, false);
     }
 
     private void FixedUpdate()
