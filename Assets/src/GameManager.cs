d588d87489b176ced3b77a9582412364f74215bf
eduardoztt <edu.zancket@gmail.com>
2025-05-05 18:12:13 -0300

    ajuste

--- Assets/src/GameManager.cs
+++ Assets/src/GameManager.cs
@@ -5,15 +5,23 @@ public class GameManager : MonoBehaviour
 {
     // Outras funcionalidades do seu GameManager podem estar aqui
 
-    public void LoadSceneByName(string sceneName)
-    {
+    public void LoadSceneByName(string sceneName){
         SceneManager.LoadScene(sceneName);
         //Debug.Log("Carregando a cena: " + sceneName);
     }
 
-    public void LoadSceneByIndex(int sceneIndex)
-    {
+    public void LoadSceneByIndex(int sceneIndex){
         SceneManager.LoadScene(sceneIndex);
         //Debug.Log("Carregando a cena com índice: " + sceneIndex);
     }
+
+        public void QuitGame(){
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+        Debug.Log("Saindo do Jogo");
+    }
+
 }
